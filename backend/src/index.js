de0061ac483fea31ec95d90cef6606c073552093
Alanio Sousa <alaniocarvalho.cs@gmail.com>
2020-03-25 04:41:55 -0300

    first commit, backend-application

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+/* quando no servifor de producao, definir o origin do cors
+  app.use(cors({
+  origin: 'http://meuapp.com'
+})); 
+*/
+
+// em desenvolvimento, caso nao se utilize o origin, todas as aplicacoes podem acessar o backend
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/* 
+Rota / Recurso
+*/
+
+/* 
+Metodos HTTP:
+
+GET:
+POST:
+PUT:
+DELETE:
+*/
+
+/* 
+Tipos de parametros
+Query Params: Parametros nomeados na rota apos "?" (Filtros, paginacao)
+Route Params: Parametros utilizados para identificar recursos
+Request Body: Corpo da requisicao utilizado para criar ou alterar recursos
+*/
